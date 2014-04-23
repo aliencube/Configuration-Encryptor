e926bd1b8d1e6d0851d2594b793ad8befcbd796e
Justin Yoo <justin.yoo@aliencube.com>
2014-04-24 08:20:27 +1000

    Added console app and sample app projects.

---
+++ SourceCodes/ConfigurationEncryptor.ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationEncryptor.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void EncryptConnectionString(bool encrypt, string fileName)
+        {
+            Configuration configuration = null;
+            try
+            {
+                // Open the configuration file and retrieve the connectionStrings section.
+                configuration = ConfigurationManager.OpenExeConfiguration(fileName);
+                ConnectionStringsSection configSection =
+                configuration.GetSection("connectionStrings") as ConnectionStringsSection;
+                if ((!(configSection.ElementInformation.IsLocked)) &&
+                    (!(configSection.SectionInformation.IsLocked)))
+                {
+                    if (encrypt && !configSection.SectionInformation.IsProtected)
+                    {
+                        //this line will encrypt the file
+                        configSection.SectionInformation.ProtectSection
+                            ("DataProtectionConfigurationProvider");
+                    }
+
+                    if (!encrypt &&
+                    configSection.SectionInformation.IsProtected)//encrypt is true so encrypt
+                    {
+                        //this line will decrypt the file. 
+                        configSection.SectionInformation.UnprotectSection();
+                    }
+                    //re-save the configuration file section
+                    configSection.SectionInformation.ForceSave = true;
+                    // Save the current configuration
+
+                    configuration.Save();
+                    Process.Start("notepad.exe", configuration.FilePath);
+                    //configFile.FilePath 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SourceCodes/ConfigurationEncryptor.SampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.ConfigurationEncryptor.SampleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e56ac68e-5c93-47da-907e-707af94c96eb")]
